1af8ff8dc1273995b8b580217d2f43d34d4687bb
BEELZ <othmanmuhidean@gmail.com>
2020-10-30 19:39:08 -0700

    retrieving a response

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ const PORT = 3005
 
 const server = http.createServer((req,res)=>{
 // console.log(http.METHODS);
-console.log(http.STATUS_CODES);  
-    
+// console.log(http.STATUS_CODES); 
+// console.log(req.headers)
+// console.log(req.url)
+
+let path = url.parse(req.url, true);
+// The only values that will be retrieved in path are:
+// path.pathname path.search path.query query string object
+
+res.writeHead(200,"OK", {"Content-Type":"text/plain"});
+res.write("The Response\n\n");
+res.end("end of Message to Browser");
+
+
 })
 
 server.listen(PORT,()=>{
